3e4acddf66c602e5ebf7d72521b66aa92e9dcc2a
1337fury <ey.abdelhalim@gmail.com>
2025-02-27 14:26:44 +0000

    feat: add constructors to AuthRequest for better testability

--- src/main/java/com/example/miniproject/dto/AuthRequest.java
+++ src/main/java/com/example/miniproject/dto/AuthRequest.java
@@ -1,9 +1,13 @@
 package com.example.miniproject.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AuthRequest {
     @NotBlank(message = "Username or email is required")
     private String username;
